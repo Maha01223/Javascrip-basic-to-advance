f5c0c75ef412f6dd7dddfa13dbd4cbf0031532bc
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-10-23 15:23:03 +0530

    add feature

--- 01_basic/06_nums_and_maths.js
+++ 01_basic/06_nums_and_maths.js
@@ -1,12 +1,30 @@
+//++++++++++++++++++++++++nums+++++++++++++++++++++++++++++++++++
+
 const score = 4000;
-console.log(score);
+// console.log(score);
 
 const balance = new Number(5212555);
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(typeof balance);
-console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
 
 const otherNumber = 1528.588;
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 10000000;
+// console.log(hundred.toLocaleString("en-IN"));
+
+//***********************Maths*******************************
+
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.955));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(5, 5, 14, 48, 3));
+// console.log(Math.max(5, 48, 100, 10000, 2555, 25));
+
+console.log(Math.random());
