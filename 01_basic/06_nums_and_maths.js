f8d0688055123f6a39b251ada89175fe6512c549
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-10-23 14:30:38 +0530

    add features

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,12 @@
+const score = 4000;
+console.log(score);
+
+const balance = new Number(5212555);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(typeof balance);
+console.log(balance.toFixed(2));
+
+const otherNumber = 1528.588;
+console.log(otherNumber.toPrecision(3));
