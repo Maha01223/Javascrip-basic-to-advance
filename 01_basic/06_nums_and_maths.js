26d07f7a0bfa80d8b86a83d0ac0b1670f01ecdc9
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-10-25 19:18:00 +0530

    feature

--- 01_basic/06_nums_and_maths.js
+++ 01_basic/06_nums_and_maths.js
@@ -37,4 +37,4 @@ const min = 10;
 
 const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min); its a formula to use min and max valve
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,2 @@
+const Arry = [0, 1, 2, 3, 4, 5, 6, true, "Mohsin"];
+console.log(Arry);
