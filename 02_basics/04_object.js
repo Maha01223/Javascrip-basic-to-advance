34a097ed96eea00ab0782dc561e4a4e43d344449
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-10-27 20:46:46 +0530

    add feature

---
+++ 02_basics/04_object.js
@@ -0,0 +1,66 @@
+// singleton say in other word constructors
+
+// const tinderUser = new Object(); // singleton object
+
+//or both way to correct
+
+const tinderUser = {}; // non singleton object
+
+tinderUser.id = "123abc";
+tinderUser.name = "sam";
+tinderUser.inLoggedIN = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "maha@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Mohsin",
+      Lastname: "Ansari",
+    },
+  },
+};
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = { 1: "mahi", 2: "Maha" };
+const obj2 = { 3: "dani", 4: "dan" };
+
+// const obj3 = { obj1, obj2 };
+
+// const obj3 = Object.assign(obj1, obj2);
+const obj3 = { ...obj1, ...obj2 };
+
+console.log(obj3);
+
+const users = [
+  {
+    id: "1",
+    email: "mahi122@gmail.com",
+  },
+
+  {
+    id: "1",
+    email: "mahi122@gmail.com",
+  },
+
+  {
+    id: "1",
+    email: "mahi122@gmail.com",
+  },
+
+  {
+    id: "1",
+    email: "mahi122@gmail.com",
+  },
+];
+
+// console.log(users[1].email);
+console.log(users);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty("inLoggedIN"));
+console.log(tinderUser.hasOwnProperty("LoggedIN"));
