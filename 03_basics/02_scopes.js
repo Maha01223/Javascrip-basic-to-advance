8f46e1787c52d40b8c32aac636e7361a3c5d4a9d
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-03 20:50:43 +0530

    features

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,50 @@ let a = 1000; // its global scope
 
 if (true) {
   // its a block scope
+
   let a = 10;
   const b = 20;
-  console.log("Inner:", a);
+  //   console.log("Inner:", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
+// ++++++++ nested ++++
+function one() {
+  const username = "Mohsin";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  //   console.log(website);
+  //   two();
+}
+
+// one();
+
+if (true) {
+  const username = "Mohsin";
+  if (username === "Mohsin") {
+    const website = " youtube";
+    console.log(username + website);
+  }
+  //   console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++++intresting++++++++++++++
+
+// console.log(addone(5));  // sirf function ko aise print kr skte hai
+function addone(num) {
+  return num + 1;
+}
+
+//++++++++++++++OR+++++++++++++++++++++
+
+// console.log(addTwo(6)); // variable function jis me koi function add ho uss ko aise print nahi  kr skte
+const addTwo = function (num) {
+  return num + 2;
+};
+
+// console.log(addTwo(6));
