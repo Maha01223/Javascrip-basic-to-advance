f45a363765ef8cc8c20053e74effcb24e28f6c8d
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-13 19:32:36 +0530

    features

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+//  IF
+//  true  condition hai to true me  code execute hoga, 2=="2"
+// false condition hai toh code execute nahi hoga, 2=== "2"
+
+if (true) {
+}
+
+if (false) {
+}
+// <,>, <=, >=, == , ===,
+
+// example
+
+const isUserloggedIn = true;
+const temp = 41;
+
+if (temp < 51) {
+  //   console.log("less then 50");
+}
+
+// console.log("greater then 50");
+
+// dono he exicute hoge aise me
+
+// if (temp === 40) {
+//   console.log("less then 50");
+// } else {
+//   console.log("greater than 50");
+// }
+
+// console.log("exicute");
+
+// const scope = 200;
+// if (scope > 100) {
+//   let power = "fly";
+//   console.log(`user power : ${power}`);
+// }
+
+// console.log(`user power : ${power}`);
+
+// var is a completely global
