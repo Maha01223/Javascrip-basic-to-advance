f55018af415b02560b35b5126ed55a9537427dd6
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-13 11:25:45 +0530

    features

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,7 +1,7 @@
 // Immeditely Invoked function Experession (IIFE)
 function one() {
   console.log(`DB CONNECTED`);
-}
+}()
 // one(); // what is iife? jo function immediate exicute ho jaaye.
 
 // global score ke polution se problem hoti hai kaye baar uss global score ke varible hai ya jo bhe decleration hai uss ke polution ko hatane ke liye IIFE use krte hai
