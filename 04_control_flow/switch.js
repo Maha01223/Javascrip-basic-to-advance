15d3dded5bd928fcc47ef8175c613cebdd06692c
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-14 20:03:17 +0530

    features

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,50 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   case 3:
+//     console.log("March");
+//   // break;
+//   case 4:
+//     console.log("Aprail");
+//     break;
+
+//   default:
+//     console.log("default case Match");
+//     break;
+// }
+
+const months = "march";
+
+switch (months) {
+  case "january":
+    console.log("January");
+    break;
+  case "february":
+    console.log("February");
+    break;
+  case "march":
+    console.log("March");
+    break;
+  case "aprail":
+    console.log("Aprail");
+    break;
+
+  default:
+    console.log("default case Match");
+    break;
+}
