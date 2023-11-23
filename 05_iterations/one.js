efb0a1579eda9f4e6086cd16d9feb7b34fac4bd6
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-23 17:55:26 +0530

    features

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -21,10 +21,30 @@
 //   }
 // }
 
-let myArray = ["flash", "batman", "superman"];
-console.log(myArray.length);
+// let myArray = ["flash", "batman", "superman"];
+// console.log(myArray.length);
 
-for (let index = 0; index < myArray.length; index++) {
-  const element = myArray[index];
-  console.log(element);
+// for (let index = 0; index < myArray.length; index++) {
+//   const element = myArray[index];
+//   console.log(element);
+// }
+
+// Break and Continous
+
+// for (let index = 1; index <= 20; index++) {
+//   const element = index;
+//   if (index == 5) {
+//     console.log(`Detected 5`);
+//     break;
+//   }
+//   console.log(element);
+// }
+
+for (let index = 0; index <= 10; index++) {
+  const element = index;
+  if (index == 5) {
+    console.log(`Detection of 5`);
+    continue;
+  }
+  console.log(`valve odf i is ${index}`);
 }
