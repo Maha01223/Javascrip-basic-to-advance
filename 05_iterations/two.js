88f144247a2dc2adf25c971e50f183d2caca783e
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-23 20:42:26 +0530

    feature

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// let index = 0;
+// while (index <= 30) {
+//   console.log(`valve of index is ${index}`);
+//   index = index + 3;
+// }
+
+let myArray = ["flash", "boot", "mannu", "denam"];
+console.log(myArray.length);
+
+let arr = 0;
+// while (arr < myArray.length) {
+//   console.log(`valve is ${myArray[arr]}`);
+//   arr = arr + 1;
+// }
+
+// same BOth as different code
+
+while (arr < myArray.length) {
+  console.log(myArray[arr]);
+  arr++;
+}
