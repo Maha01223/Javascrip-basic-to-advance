b341931f046e0b8eed0f77ce316b7217b044faf9
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-23 17:44:28 +0530

    features

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -5,7 +5,26 @@
 //   console.log(element);
 // }
 
-for (let i = 0; i <= 20; i++) {
-  const element = i;
-  console.log(i);
+// for (let i = 1; i <= 20; i++) {
+//   const element = i;
+//   if (i == 5) {
+//     console.log("5 is Best Number");
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`Outer Loop : ${i}`);
+//   for (let j = 1; j <= 10; j++) {
+//     // console.log(`Inner Loop Valve ${j} and inner valve ${i}`);
+//     console.log(i + "*" + j + " = " + i * j);
+//   }
+// }
+
+let myArray = ["flash", "batman", "superman"];
+console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
 }
