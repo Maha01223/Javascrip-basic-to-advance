f4cc6e25a1ffd554dadae54fc9a03d9604f921f4
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-24 01:15:50 +0530

    features

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -14,3 +14,38 @@
 // }
 
 //  Map
+
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("Fr", "France");
+map.set("In", "India");
+
+// console.log(map);
+
+for (const [key, value] of map) {
+  //   console.log(key, ":-", value);
+}
+
+const myObject = {
+  game: "NFS",
+  game1: "Spiderman",
+};
+
+// console.log(myObject);
+// object me forof method is not working, its on;y forin is working
+
+// for (const [key, value] of myObject) {
+//   console.log(key, ":-", value);
+// }
+
+const mYObject = {
+  js: "javascript",
+  cpp: "c++",
+  rb: "ruby",
+  swift: "swift bt apple",
+};
+
+for (const key in mYObject) {
+  console.log(key);
+}
