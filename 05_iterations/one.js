f6b343fbfd57da8c64db9d951d3db6090d387883
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-11-23 16:44:36 +0530

    featutres

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,10 +1,11 @@
 // for loops
 
-for (let index = 0; index <= 10; index++) {
-  const element = index;
-  console.log(element);
+// for (let index = 0; index <= 10; index++) {
+//   const element = index;
+//   console.log(element);
+// }
+
+for (let i = 0; i <= 20; i++) {
+  const element = i;
+  console.log(i);
 }
- 
-
-
-*****1111111******
