8ce33ec0604f82900001a4ca3c0f8ccfff05d44e
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-12-07 18:07:57 +0530

    features

---
+++ 07_project/02_project/index.js
@@ -0,0 +1,21 @@
+console.log("Mohsin Ansari");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const result = document.querySelector("#results");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    result.innerHTML = `please give a valid height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    result.innerHTML = `please give me a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 1000)).toFixed(2);
+    // show the reults
+    result.innerHTML = `<span>${bmi}</span>`;
+  }
+});
