bfd184ac66e384d191a2c3cd9ecd63088b37bdb8
Mohsin Ansari <mohsin.gani78692@gmail.com>
2023-12-07 18:29:34 +0530

    features

--- 07_project/02_project/index.js
+++ 07_project/02_project/index.js
@@ -15,7 +15,17 @@ form.addEventListener("submit", function (e) {
     result.innerHTML = `please give me a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 1000)).toFixed(2);
+
+    let weightType = "";
+    if (bmi < 18.6) {
+      weightType = "under wieght";
+    } else if (bmi > 18.6 && bmi < 24.9) {
+      weightType = "Normal Weight";
+    } else {
+      weightType = "OverWeight";
+    }
+
     // show the reults
-    result.innerHTML = `<span>${bmi}</span>`;
+    result.innerHTML = `<span>Persons BMI is ${bmi} so it is ${weightType}</span>`;
   }
 });
